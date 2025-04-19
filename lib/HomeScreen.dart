a881d0a8053d382a6ce9f9f39b608169d7327e56
sam61988 <samsamoy32@gmail.com>
2025-04-19 23:07:40 +0300

    init project.

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  static const String routename = 'Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QuranApp'),
+      ),
+    );
+  }
+}
